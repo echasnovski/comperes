920cfde61223d45207b0a2e4c8c9d82a44bb93d9
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2018-05-01 15:53:06 +0300

    Add `hp_survey` data.

--- R/data.R
+++ R/data.R
@@ -7,3 +7,55 @@
 #' @format An object of class [longcr] containing information
 #'   about 10 games.
 "ncaa2005"
+
+#' Results of Harry Potter Books Survey
+#'
+#' `hp_survey` contains results of the survey with a goal to collect data enough
+#' to rate Harry Potter books.
+#'
+#' @details Survey was done via
+#' [Google Forms](https://www.google.com/intl/en/forms/about/) service. To
+#' participate in it, respondent is asked to log in into her/his Google account
+#' (to ensure that one person takes part only once). It was popularized mostly
+#' among R users via [R-bloggers](https://www.r-bloggers.com/) and
+#' [Twitter](https://twitter.com/).
+#'
+#' At the beginning of the survey, there was the following text:
+#'
+#' _This is a survey with goal to collect data enough to rate Harry Potter books.
+#' Data will be made public with complete anonymity of respondents. Please, take
+#' part only if you have read all seven original J. K. Rowling Harry Potter
+#' books and are willing to give an honest feedback about your impressions._
+#'
+#' Analyzed books were coded with the following names:
+#' - “HP and the Philosopher’s (Sorcerer’s) Stone (#1)”.
+#' - “HP and the Chamber of Secrets (#2)”.
+#' - “HP and the Prisoner of Azkaban (#3)”.
+#' - “HP and the Goblet of Fire (#4)”.
+#' - “HP and the Order of the Phoenix (#5)”.
+#' - “HP and the Half-Blood Prince (#6)”.
+#' - “HP and the Deathly Hallows (#7)”.
+#'
+#' Survey had the following procedure:
+#' - At first, respondent is asked to choose the first element in the randomly
+#' shuffled list of number from 1 to 127. This simulates the random generation
+#' of books subset in the next question.
+#' - Next he/she is presented with a question "What is your impression of these
+#' Harry Potter BOOKS?" (singular if there is one book) and the following
+#' question grid:
+#'     - Rows represent randomly shuffled subset of books corresponding to the
+#'     number chosen in the first step.
+#'     - Columns contain the following scale of answers: “1 - Poor”, “2 - Fair”,
+#'     “3 - Good”, “4 - Very Good”, “5 - Excellent”. Respondent is asked and
+#'     allowed to choose only one answer per book (every book should be rated).
+#'
+#' @format A [tibble][tibble::tibble] with answers from 181 respondents and the
+#'   following columns:
+#' - __person__ <int>: Identifier of a person.
+#' - __book__ <chr>: Identifier of a Harry Potter book. Its values are of the
+#' form "HP_\*" where "\*" represents book's number in the series (from 1 to 7).
+#' - __score__ <chr>: Book's score. Can be one of "1 - Poor", "2 - Fair", "3 -
+#' Good", "4 - Very Good", "5 - Excellent".
+#'
+#' Rows are ordered by person and then by book identifier.
+"hp_survey"
---
+++ data-raw/hp_survey.R
@@ -0,0 +1,72 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+
+# Functions ---------------------------------------------------------------
+append_ind <- function(x, ind) {
+  lapply(x, function(i) {
+    c(i, ind)
+  })
+}
+
+compute_batch_num <- function(n) {
+  n <- as.integer(n)
+  if (n <= 1) {
+    return(1)
+  }
+
+  # This is how books are generated in the original survey form
+  batches <- list(1)
+  for (i in seq_len(n-1) + 1) {
+    batches <- c(batches, list(i), append_ind(batches, i))
+  }
+
+  # Compute batch numbers
+  batch_len <- sapply(batches, length)
+
+  unlist(mapply(rep, seq_along(batches), batch_len, SIMPLIFY = FALSE))
+}
+
+hp_repair_colnames <- function(tbl) {
+  books <- colnames(tbl)[-(1:2)]
+  batch_num <- compute_batch_num(n = 7)
+  books_out <- paste0(
+    batch_num, "-",
+    gsub("^[^0-9]*([1-7]).*$", "HP_\\1", books)
+  )
+
+  res_names <- c("time", "batch", books_out)
+  colnames(tbl) <- res_names
+
+  tbl
+}
+
+
+# Data creation -----------------------------------------------------------
+hp_survey <- read.csv(
+  file = file.path("data-raw", "hp_survey.csv"),
+  header = TRUE, stringsAsFactors = FALSE
+) %>%
+  as_tibble() %>%
+  mutate_all(as.character) %>%
+  mutate(
+    Time = as.POSIXct(
+      Time, format = "%Y/%m/%d %I:%M:%S %p", tz = "Europe/Kiev"
+    ),
+    Time = lubridate::with_tz(Time, "UTC")
+  ) %>%
+  hp_repair_colnames() %>%
+  mutate(person = seq_len(n())) %>%
+  gather("batch_book", "score", -time, -batch, -person) %>%
+  filter(!((score == "") | is.na(score))) %>%
+  separate(batch_book, into = c("new_batch", "book"), sep = "-")
+
+# Test for correct batch number imputation. Should be TRUE
+hp_survey %>% summarise(all_batch_equal = all(batch == new_batch))
+
+hp_survey <- hp_survey %>%
+  select(person, book, score) %>%
+  arrange(person, book)
+
+save(hp_survey, file = file.path("data", "hp_survey.rda"), compress = "bzip2")
